259c2faa67b0d289fc6cc2330750f14fe652d2fc
131764826+MARTIN-339@users.noreply.github.com <muirujnr@gmail.com>
2023-10-24 15:06:08 -0700

    printf value

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-printf
+printf it is
---
+++ _putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - print outs a character to the standard output
+ * @ch: expected character to write
+ * Return: -1 on error and 1 on success
+ */
+int _putchar(char ch)
+{
+	return (write(1, &ch, 1));
+}
